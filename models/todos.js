8023a884162129539b771713bea8f367275d79e5
gonpeche <espeche.g@gmail.com>
2018-10-01 15:12:16 -0300

    initial commit

--- models/todos.js
+++ models/todos.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var tasks = {}; // acá vamos a guardar nuestras personas y tareas
+var tasks = {
+    people: []
+}; // acá vamos a guardar nuestras personas y tareas
+
 
 module.exports = {
   reset: function () {
@@ -9,8 +12,10 @@ module.exports = {
   // ==== COMPLETEN LAS SIGUIENTES FUNCIONES (vean los test de `model.js`) =====
   listPeople: function () {
     // devuelve un arreglo de personas con tareas
+    return tasks.people
   },
   add: function (name, task) {
+    tasks.people.push(name)
     // guarda una tarea para una persona en particular
   }
   // etc.
